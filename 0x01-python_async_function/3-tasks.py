c6b3659d36f57063c1f4c175ca228548a8fba8e9
Celestine Akpanoko <celestineakpanoko@gmail.com>
2022-04-18 21:38:15 +0100

    returns an asyncio task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env Python3
+'''A script that returns a asyncio task'''
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    '''
+    Returns an asyncio task task after a random number of seconds
+    Args:
+        max_delay: maximum delay in seconds
+    Returns: an asyncio.Task object
+    '''
+    return asyncio.create_task(wait_random(max_delay))
